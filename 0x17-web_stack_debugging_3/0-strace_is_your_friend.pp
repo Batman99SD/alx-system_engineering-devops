a3528f5e918fef49b96f5938b6181e6fc3bc5a28
Batman99SD <azozkrg1@gmail.com>
2024-06-05 14:15:17 -0700

    pup

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #fixing server error 500
 
 exec { 'wp-fix':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => ['/usr/bin/', '/bin', '/usr/local/bin/']
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
